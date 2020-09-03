a8365e9e9d4b725264aaa5bab245a4373ca4d936
Liying Yin <yinliying53@gmail.com>
2020-09-02 22:02:48 -0700

    Stage 1, update components, craw data from nba website

---
+++ src/components/TopBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/images/logo.svg';
+
+class TopBar extends React.Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        );
+    }
+}
+
+export default TopBar;
