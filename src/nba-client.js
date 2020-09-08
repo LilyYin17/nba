2ed7df10ad9d9c3c98f8ffcb30d83e099f850ebd
Liying Yin <yinliying53@gmail.com>
2020-09-08 15:14:23 -0700

    Stage 1, update components, craw data from nba website

--- src/nba-client.js
+++ src/nba-client.js
@@ -1,8 +1,7 @@
-//fetch data from nba.stats website
 import nba from 'nba';
 
-const SERVER_URL = 'https://nba.laiprojects.com';
-// const SERVER_URL = 'http://35.235.84.235:5000';
+//const SERVER_URL = 'https://nba.laiprojects.com/';
+const SERVER_URL = 'http://35.235.84.235:5000';
 export default {
     ...nba,
     stats: {
